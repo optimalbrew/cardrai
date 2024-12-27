4f89839e70b2cc5e27e6d011e4c4c7b526d393f6
optimalbrew <petecarkeek@gmail.com>
2024-12-26 17:45:06 -0800

    add tables, code clean up

--- src/App.js
+++ src/App.js
@@ -16,134 +16,69 @@ const Calculator = () => {
 
   //Age points
   const valueMappingAge = {
-    option1: 52,
-    option2: 27,
-    option3: 0,
+    "Less than 3 months": 52,
+    "3-24 months": 27,
+    "More than 24 months": 0,
   };
 
   //STAT score points
   const valueMappingStat = {
-    option1: 0,
-    option2: 33,
-    option3: 53,
-    option4: 65,
-    option5: 96,
-    option6: 87,
+    "1": 0,
+    "2": 33,
+    "3": 53,
+    "4": 65,
+    "5": 96,
+    "Unknown": 87,
   }; 
 
   //STAT score points .. these are not linear and the input values jump at the end of the range
   let valueMappingVIS = {
-    "0": 0,
-    "1": 11,
-    "2": 22,
-    "3": 33,
-    "4": 44,
-    "5": 55,
-    "6": 65,
-    "7": 74,
-    "8": 81,
-    "9": 87,
-    "10": 92,
-    "11": 95,
-    "12": 97,
-    "13": 99,
-    "14": 100,
-    "15": 100,
-    "20": 99,
-    "25": 98,
-    "30": 97,
+    0: 0,
+    1: 11,
+    2: 22,
+    3: 33,
+    4: 44,
+    5: 55,
+    6: 65,
+    7: 74,
+    8: 81,
+    9: 87,
+    10: 92,
+    11: 95,
+    12: 97,
+    13: 99,
+    14: 100,
+    15: 100,
+    20: 99,
+    25: 98,
+    30: 97,
   };
-
-  //treat values in above mapping between 16 and 19 as 15
-  for (let i = 16; i < 20; i++) {
-    valueMappingVIS[i] = valueMappingVIS["15"];
-  }
-  //and treat values in above mapping between 21 and 24 as 20
-  for (let i = 21; i < 25; i++) {
-    valueMappingVIS[i] = valueMappingVIS["20"];
-  }
-  //and treat values in above mapping between 26 and 29 as 25
-  for (let i = 26; i < 30; i++) {
-    valueMappingVIS[i] = valueMappingVIS["25"];
-  }
-
 
   //Fluid balance points
   let valueMappingFluidBal = {
-    "0": 0,
-    "5": 0,
-    "10": 1,
-    "15": 2,
-    "20": 4,
-    "25": 5,
-    "30": 7,
-    "35": 9,
-    "40": 11,
-    "45": 13,
-    "50": 15,
-    "55": 17,
-    "65": 21,
-    "75": 26,
+    0: 0,
+    5: 0,
+    10: 1,
+    15: 2,
+    20: 4,
+    25: 5,
+    30: 7,
+    35: 9,
+    40: 11,
+    45: 13,
+    50: 15,
+    55: 17,
+    65: 21,
+    75: 26,
   }
 
-  //treat values in above mapping between 1 and 4 as 0
-  for (let i = 1; i < 5; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["0"];
-  }
-  //and treat values in above mapping between 6 and 9 as 5
-  for (let i = 6; i < 10; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["5"];
-  }
-  //and treat values in above mapping between 11 and 14 as 10
-  for (let i = 11; i < 15; i++) { 
-    valueMappingFluidBal[i] = valueMappingFluidBal["10"];
-  }
-  //and treat values in above mapping between 16 and 19 as 15
-  for (let i = 16; i < 20; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["15"];
-  }
-  //and treat values in above mapping between 21 and 24 as 20
-  for (let i = 21; i < 25; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["20"];
-  }
-  //and treat values in above mapping between 26 and 29 as 25
-  for (let i = 26; i < 30; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["25"];
-  }
-  //and treat values in above mapping between 31 and 34 as 30
-  for (let i = 31; i < 35; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["30"];
-  }
-  //and treat values in above mapping between 36 and 39 as 35
-  for (let i = 36; i < 40; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["35"];
-  }
-  //and treat values in above mapping between 41 and 44 as 40
-  for (let i = 41; i < 45; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["40"];
-  }
-  //and treat values in above mapping between 46 and 49 as 45
-  for (let i = 46; i < 50; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["45"];
-  }
-  //and treat values in above mapping between 51 and 54 as 50
-  for (let i = 51; i < 55; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["50"];
-  }
-  //and treat values in above mapping between 56 and 64 as 55
-  for (let i = 56; i < 65; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["55"];
-  }
-  //and treat values in above mapping between 66 and 75 as 65
-  for (let i = 66; i < 75; i++) {
-    valueMappingFluidBal[i] = valueMappingFluidBal["65"];
-  }
+  
 
   //CBP duration points
   const valueMappingCBP = {
-    option1: 0,
-    option2: 24,
-    option3: 58,
+    "Less than 180 minutes": 0,
+    "More than 180 minutes": 24,
+    "Unknown": 58,
   };
 
 
@@ -167,8 +102,26 @@ const Calculator = () => {
 
     const mappedValueAge = valueMappingAge[Age] || 0;
     const mappedValueStat = valueMappingStat[Stat] || 0;
-    const intVIS8 = valueMappingVIS[parseInt(VIS8Hr, 10)] || 0;
-    const intFluidBal = valueMappingFluidBal[parseInt(FluidBal, 10)] || 0;
+    
+    const parsedKeyVIS8 = parseInt(VIS8Hr, 10);  
+
+    //if parsedkey is not in the keys for valuemappingVIS8, find the largest key that is less than parsedkey
+    let intVIS8 = 0;
+    for (let key in valueMappingVIS) {
+      if (parsedKeyVIS8 >= key) {
+        intVIS8 = valueMappingVIS[key];
+      }
+    }  
+
+    //do the same for fluid balance
+    const parsedKeyFBal = parseInt(FluidBal, 10);  
+    let intFluidBal = 0;
+    for (let key in valueMappingFluidBal) {
+      if (parsedKeyFBal >= key) {
+        intFluidBal = valueMappingFluidBal[key];
+      }
+    }
+
     const mappedValueCBP = valueMappingCBP[CBP] || 0;
 
     const calculationResult = mappedValueAge + mappedValueStat + intVIS8 + intFluidBal + mappedValueCBP;
@@ -181,35 +134,36 @@ const Calculator = () => {
        <div className="p-4 border rounded shadow-sm bg-light">
   
       <form  onSubmit={handleSubmit}>
-        <h3>Nomogram Variables</h3>
+        <h2>Nomogram Variables</h2>
+        <p>Enter the values to calculate total points. Reference tables for overall risk score and points system appear below.</p>
       
         <label className="form-label">
-          Age:
+          Age
           <select className="form-select" value={Age} onChange={(e) => setAge(e.target.value)}>
             <option value="">Select an option</option>
-            <option value="option1"> Less than 3 months</option>
-            <option value="option2">3-24 months</option>
-            <option value="option3"> More than 24 months</option>
+            <option value="Less than 3 months"> Less than 3 months</option>
+            <option value="3-24 months">3-24 months</option>
+            <option value="More than 24 months"> More than 24 months</option>
           </select>
         </label>
         <br />
     
         <label className="form-label">
-          STAT Score:
+          STAT Score
           <select className="form-select" value={Stat} onChange={(e) => setStat(e.target.value)}>
             <option value="">Select an option</option>
-            <option value="option6">Unknown</option>
-            <option value="option1">1</option>
-            <option value="option2">2</option>
-            <option value="option3">3</option>
-            <option value="option4">4</option>
-            <option value="option5">5</option>
+            <option value="Unknown">Unknown</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
           </select>
         </label>
         <br />
 
         <label className="form-label">
-          VIS at 8 hours (from 0 to 30):
+          VIS at 8 hours (from 0 to 30)
           <input className="form-control"
             type="number"
             value={VIS8Hr}
@@ -221,7 +175,7 @@ const Calculator = () => {
         <br />
         
         <label className="form-label">
-        Fluid balance on POD 0, mL/kg (from 0 to 75):
+        Fluid balance on POD 0, mL/kg (from 0 to 75)
           <input className="form-control"
             type="number"
             value={FluidBal}
@@ -233,12 +187,12 @@ const Calculator = () => {
         <br />
 
         <label className="form-label">
-          CBP Duration:
+          CBP Duration
           <select className="form-select" value={CBP} onChange={(e) => setCBP(e.target.value)}>
             <option value="">Select an option</option>
-            <option value="option1"> Less than 180 minutes</option>
-            <option value="option2">More than 180 minutes</option>
-            <option value="option3"> Unknown</option>
+            <option value="Less than 180 minutes"> Less than 180 minutes</option>
+            <option value="More than 180 minutes">More than 180 minutes</option>
+            <option value="Unknown"> Unknown</option>
           </select>
         </label>
         <br />
@@ -246,7 +200,7 @@ const Calculator = () => {
         <button className="btn btn-primary w-100" type="submit"> Calculate</button>
       </form>
       
-      {result !== null && <h3>Points: {result}</h3>}
+      {result !== null && <h3>Total points: {result}</h3>}
     </div>
 
     <div className="p-4 border rounded shadow-sm bg-light">
@@ -268,6 +222,116 @@ const Calculator = () => {
         ))}
       </tbody>
     </table>
+    </div>
+
+    <div className="p-4 border rounded shadow-sm bg-light">
+      <h2>Reference for points</h2>
+      
+      <div>
+        <h3>Points for Age</h3>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Age</th>
+              <th>Points</th>
+            </tr>
+          </thead>     
+
+          <tbody>
+            {Object.entries(valueMappingAge).map(([key, value]) => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <h3>Points for STAT Score</h3>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>STAT Score</th>
+              <th>Points</th>
+            </tr>
+          </thead>     
+
+          <tbody>
+            {Object.entries(valueMappingStat).map(([key, value]) => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      
+      <div>
+        <h3>Points for VIS at 8 hours</h3>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>VIS at 8 hours</th>
+              <th>Points</th>
+            </tr>
+          </thead>     
+
+          <tbody>
+            {Object.entries(valueMappingVIS).map(([key, value]) => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <h3>Points for Fluid Balance on POD 0, mL/kg</h3>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Fluid Balance</th>
+              <th>Points</th>
+            </tr>
+          </thead>     
+
+          <tbody>
+            {Object.entries(valueMappingFluidBal).map(([key, value]) => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <h3>Points for CBP Duration</h3>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>CBP Duration</th>
+              <th>Points</th>
+            </tr>
+          </thead>     
+
+          <tbody>
+            {Object.entries(valueMappingCBP).map(([key, value]) => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
     </div>    
 
     </div>
